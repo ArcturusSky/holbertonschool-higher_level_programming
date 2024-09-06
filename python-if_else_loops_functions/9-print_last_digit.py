0fedf7f869afcedc39045f4da3703d85d05e1fb3
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-09-06 11:37:46 +0200

    Refactor: Improve print_last_digit() function
    - Added digit = digit * -1 to convert in positiv the last digit
    - Added the return value

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,6 +2,8 @@
 def print_last_digit(number):
     if number < 0:
         digit = number % -10
+        digit = digit * -1
     else:
         digit = number % 10
     print("{}".format(digit), end="")
+    return(digit)
