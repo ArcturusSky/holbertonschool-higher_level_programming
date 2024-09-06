1481a60a92112fcf2457797607c66247642d470e
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-09-06 11:22:51 +0200

    Feat: Implement uppercase() function to convert string to uppercase
    - Created uppercase() function that takes a string as input
    - Iterated through each character in the input string
    - Converted lowercase letters to uppercase using ASCII manipulation
    - Preserved non-lowercase characters
    - Printed the resulting uppercase string

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    upper_string = ""
+    for char in str:
+        ascii_numb = ord(char)
+        if ascii_numb in range(97, 123):
+            upper_string += chr(ascii_numb - 32)
+        else:
+            upper_string += char
+    print(upper_string)
