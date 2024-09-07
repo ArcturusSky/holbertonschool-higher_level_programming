f365dc674bb7366bb15376ea367df72fba7815fe
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-09-07 20:41:01 +0200

    Refactor: Improve output consistency in command-line argument handler
    - Updated output for zero arguments case to use format string
    - Simplified single argument case to use consistent formatting
    - Retained logic for multiple arguments case
    - Ensured consistent use of str.format() method throughout

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,10 +5,10 @@ if __name__ == "__main__":
     args = len(sys.argv) - 1
     
     if args == 0:
-        print("0 arguments.")
+        print("{} argument.".format(args))
     
     elif args == 1:
-        print("1 argument:")
+        print("{} argument:".format(args))
         print("1: {}".format(sys.argv[1]))
 
     else:
