990c31c8d7cf0296ad5fdd844298a1cc2046a845
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-09-10 10:03:17 +0200

    Refactor: Simplify add_tuple function and remove unnecessary checks
    - Removed redundant None check in tuple_b processing
    - Conforming to pycodestyle

--- python-data-structures/7-add_tuple.py
+++ python-data-structures/7-add_tuple.py
@@ -7,20 +7,16 @@ def add_tuple(tuple_a=(), tuple_b=()):
         while len(my_lista) < 2:        # Append 0 until correct len
             my_lista.append(0)
 
-        tuple_a = tuple([my_lista[0], my_lista[1]]) # Convert back to tuple
+        tuple_a = tuple([my_lista[0], my_lista[1]])     # Convert back to tuple
 
     if len(tuple_b) < 2:                # Same for tuple_b
-        my_listb = list(tuple_b)        
+        my_listb = list(tuple_b)
 
         while len(my_listb) < 2:
             my_listb.append(0)
 
-        for index in range (0, 1):
-            if my_listb[index] == None:
-                my_listb[index] = 0
+        tuple_b = tuple([my_listb[0], my_listb[1]])  # Convert back to tuple
 
-        tuple_b = tuple([my_listb[0], my_listb[1]]) # Convert back to tuple
-    
     # Unpack into var, add each, convert back to tuple
     a1, b1 = tuple_a
     a2, b2 = tuple_b
