370e4b56a0f1887d14979ae22137a565ad5b4e78
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-09-10 12:06:26 +0200

    Fix: Correct index range check in new_in_list function
    - Updated upper bound check to use 'len(my_list) - 1' instead of 'len(my_list)'
    - Ensures accurate handling of valid index range

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     my_list_copy = my_list.copy()
     if idx < 0:
         return my_list_copy
-    if idx > len(my_list):
+    if idx > len(my_list) - 1 :
         return my_list_copy
     else:
         my_list_copy[idx] = element
