9d8b86e4a5771debde461b071cdefca6dc909f46
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-09-10 12:14:10 +0200

    Refactor: Adjust tuple length checks in add_tuple function
    - Modified length checks to use 'len(tuple) - 1 < 2' instead of 'len(tuple) < 2'
    - Applied this change consistently for both tuple_a and tuple_b

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
 
-    if len(tuple_a) < 2:                # Check if enough item  in tuple
+    if len(tuple_a) - 1 < 2:                # Check if enough item  in tuple
         my_lista = list(tuple_a)        # Convert to list to be mutable
 
-        while len(my_lista) < 2:        # Append 0 until correct len
+        while len(my_lista) - 1 < 2:        # Append 0 until correct len
             my_lista.append(0)
 
         tuple_a = tuple([my_lista[0], my_lista[1]])     # Convert back to tuple
 
-    if len(tuple_b) < 2:                # Same for tuple_b
+    if len(tuple_b) - 1 < 2:                # Same for tuple_b
         my_listb = list(tuple_b)
 
-        while len(my_listb) < 2:
+        while len(my_listb) - 1 < 2:
             my_listb.append(0)
 
         tuple_b = tuple([my_listb[0], my_listb[1]])  # Convert back to tuple
