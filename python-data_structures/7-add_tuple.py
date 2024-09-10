c7dd2ba165275ffc5496fd10ddfa200507a347d7
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-09-10 13:08:13 +0200

    Refactor: Improve tuple handling in add_tuple function
    - Reverted length checks to original 'len(tuple) < 2' condition
    - Added slicing to ensure only first two elements of each tuple are used

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
 
-    if len(tuple_a) - 1 < 2:                # Check if enough item  in tuple
+    if len(tuple_a) < 2:                # Check if enough item  in tuple
         my_lista = list(tuple_a)        # Convert to list to be mutable
 
-        while len(my_lista) - 1 < 2:        # Append 0 until correct len
+        while len(my_lista) < 2:        # Append 0 until correct len
             my_lista.append(0)
 
         tuple_a = tuple([my_lista[0], my_lista[1]])     # Convert back to tuple
 
-    if len(tuple_b) - 1 < 2:                # Same for tuple_b
+    if len(tuple_b) < 2:                # Same for tuple_b
         my_listb = list(tuple_b)
 
-        while len(my_listb) - 1 < 2:
+        while len(my_listb) < 2:
             my_listb.append(0)
 
         tuple_b = tuple([my_listb[0], my_listb[1]])  # Convert back to tuple
 
-    # Unpack into var, add each, convert back to tuple
-    a1, b1 = tuple_a
-    a2, b2 = tuple_b
+    # Unpack only the two first into var, add each, convert back to tuple
+    a1, b1 = tuple_a[0:2]
+    a2, b2 = tuple_b[0:2]
     a = a1 + a2
     b = b1 + b2
     tuple_ab = (a, b)
