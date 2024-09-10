563c14bc1d48ba8e835facdfc78a108e32972659
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-09-10 10:02:06 +0200

    Fix - change name value for pyscodestyle

--- python-data-structures/5-no_c.py
+++ python-data-structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_string_list = list(my_string)
-    values_to_remove = {"c", "C"}
-    new_string_list = [item for item in new_string_list if item not in values_to_remove]
+    rem_c = {"c", "C"}
+    new_string_list = [item for item in new_string_list if item not in rem_c]
     new_string = ''.join(new_string_list)
     return new_string
