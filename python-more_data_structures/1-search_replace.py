cad0ed56e6bb9fcf3d3f9ea1975d77d3ab536527
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-09-11 11:08:11 +0200

    Feat: Implement search_replace function for list element replacement
    - Created function to replace specific elements in a list
    - Used loop to iterate through list indices
    - Implemented in-place replacement of matching elements
    - Returned modified list after replacements

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    for index in range(len(my_list)):
+
+        if my_list[index] == search:
+            my_list[index] = replace
+
+    return my_list
