9d7626594609d44fc2fc720d9bd97fe264450ceb
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-09-11 17:42:22 +0200

    Typo fix + adding comments

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    
+
+    # Check if empty. Warning "None" check if exactly None.
     if not a_dictionary:
         return None
     else:
+
+        # return the key corresponding to max
         return max(a_dictionary, key=a_dictionary.get)
