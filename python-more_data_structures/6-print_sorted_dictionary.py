07bdfd0640d1e8d1140e57806f76bb87f3b81aa6
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-09-11 12:59:42 +0200

    Refactor: Optimize print_sorted_dictionary function
    - Simplified function to directly iterate over sorted dictionary keys
    - Removed unnecessary steps of key extraction and list conversion

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
 
-    keys = a_dictionary.keys()      # Retrieving each key
-    orderering = list(keys)         # Converting to list
-    orderering.sort                 # Ordering list
-
-    for key in orderering:          # Print each key in ordered list
+    for key in sorted(a_dictionary):          # Print each key in ordered list
         print("{}: {}".format(key, a_dictionary[key]))
