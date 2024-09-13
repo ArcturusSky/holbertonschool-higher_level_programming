4350c1a90ba02f2a810799c9c604cf74c969d015
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-09-13 10:55:47 +0200

    Feat: Implement safe_print_list function for controlled list printing
    - Created function to safely print a specified number of list elements
    - Handled potential IndexError when accessing list elements
    - Implemented counter to track number of successfully printed elements
    - Ensured proper formatting of printed elements as integers
    - Returned count of printed elements

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    all_print = 0
+    for index in range(0, x):
+        try:
+            element = (my_list[index])
+            
+        except IndexError:
+            break
+        
+        print("{:d}".format(element), end="")
+        all_print = all_print + 1
+    
+    print()
+    return all_print
