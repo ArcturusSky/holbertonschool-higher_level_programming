a0fff99c9a824adc09e3a4097014f5d12f074a1f
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-09-13 13:10:51 +0200

    fix typo for pycodestyle

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,11 +5,11 @@ def safe_print_list(my_list=[], x=0):
         try:
             element = (my_list[index])
             print("{:d}".format(element), end="")
-            
+
         except IndexError:
             break
 
         all_print = all_print + 1
-    
+
     print()
     return all_print
