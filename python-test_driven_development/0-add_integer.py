c93d0c442d8487aeaf43f57162bf157ff343b7f7
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-09-15 16:44:52 +0200

    Feat: Implement add_integer function with robust error handling
    - Created function to add two numbers with type checking
    - Implemented checks for integer and float inputs
    - Added handling for infinite and NaN values
    - Included type casting from float to int before addition
    - Provided docstrings for module and function documentation

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -20,10 +20,29 @@ def add_integer(a, b=98):
         TypeError: If a or b is not an integer or float.
     """
 
+# Error if not int or float
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
+# Error if infinite or -infinite
+
+    if a == float('inf') or a == float('-inf'):
+        raise OverflowError
+    if b == float('inf') or b == float('-inf'):
+        raise OverflowError
+
+
+# Error if "NaN"
+
+    if a != a:
+        raise ValueError
+    if b != b:
+        raise ValueError
+
+# If everything correct, cast in int
 
     if isinstance(a, float):
         a = int(a)
