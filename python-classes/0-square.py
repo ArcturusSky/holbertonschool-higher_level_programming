f1ca95700817f728c928b83ca94de9406471b809
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-09-16 14:36:14 +0200

    Feat: Implement initial Square class
    - Created a new module for class understanding
    - Defined a basic Square class with pass statement
    - Added docstrings for module and class documentation

---
+++ python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module provides the first step into creating
+and understanding classes.
+
+"""
+
+
+class Square:
+    """
+    Creating a class "Square" to define a square
+    """
+    pass
