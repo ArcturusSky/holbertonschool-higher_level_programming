ce1d78c90af08a2a82fa46043178946b32a872b4
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-09-17 10:23:28 +0200

    Feat: Print decimal and hexadecimal equivalents
    - Implemented a for loop to iterate through numbers 0 to 98
    - Used f-string to format output with decimal and hexadecimal representations
    - Utilized hex() function to convert decimal to hexadecimal

---
+++ python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module provides the first step into creating
+and understanding classes.
+
+"""
+
+
+class Square:
+    """
+    Creating a class "Square" to define a square
+    """
+    def __init__(self, size=0):
+        """
+        Initializes the square with a private size attribute
+        """
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """
+        Calculate and return the area of the square
+        """
+        return self.__size * self.__size
