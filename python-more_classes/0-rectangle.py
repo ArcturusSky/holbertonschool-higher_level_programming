98861e4240ddebd29635c2866c88774833d70065
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-09-19 10:09:42 +0200

    Feat: Implement initial Rectangle class
    - Created a new module for class understanding
    - Defined a basic Rectangle class with pass statement
    - Added docstrings for module and class documentation

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module provides the first step into creating
+and understanding classes.
+
+"""
+
+
+class Rectangle:
+    """
+    Creating a class "Rectangle" to define a rectangle
+    """
+    pass
