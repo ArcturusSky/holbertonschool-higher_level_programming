4e17dbf534641f6c1412c3534cb3b6f4fb5014cd
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-09-19 11:02:52 +0200

    Refactor: Improve Rectangle class initialization and attribute order
    - Fixed height parameter in __init__ to have correct default value of 0
    - Reordered attribute initialization to set height before width
    - Maintained getter and setter methods for both width and height

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -10,23 +10,23 @@ class Rectangle:
     Creating a class "Rectangle" to define a rectangle
     """
 
-    def __init__(self, width=0, height=(0, 0)):
+    def __init__(self, width=0, height=0):
         """
         Initializes the rectangle with:
 
-        - a private width attribute
-        - a privtate height attribute
+        - a private height attribute
+        - a privtate width attribute
         """
 
-        self.width = width
         self.height = height
+        self.width = width
 
 # Getting and setting width before anything to allow checks first
 
     @property
     def width(self):
         """
-        Getter for the private width attribute.
+        Getter for the private width attribute
         """
 
         return self.__width
@@ -43,14 +43,14 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
 
-        self.__width = value     # Assign the validated value
+        self.__width = value
 
 # Getting and setting height before anything to allow checks first
 
     @property
     def height(self):
         """
-        Getter for the private height attribute
+        Getter for the private height attribute.
         """
 
         return self.__height
@@ -67,7 +67,9 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
 
-        self.__height = value
+        self.__height = value     # Assign the validated value
+
+
 
 # Once all checks done in setters, pursuing with methods
 
@@ -76,7 +78,7 @@ class Rectangle:
 #       Calculate and return the area of the rectangle
 #        """
 
-#        return self.width * self.height
+#        return self.height * self.width
 
 #
 #    def my_print(self):
@@ -86,17 +88,17 @@ class Rectangle:
 #        """
 #
 #        # If empty only a empty line
-#        if self.width == 0:
+#        if self.height == 0:
 #            print("")
 #
 #        else:
-#            if self.height[1] > 0:
-#                for index in range(0, self.height[1]):
+#            if self.width[1] > 0:
+#                for index in range(0, self.width[1]):
 #                    print("")
 #
-#            for firstindex in range(0, self.width):
-#                print(" " * self.height[0], end="")
-#                for secondindex in range(0, self.width):
+#            for firstindex in range(0, self.height):
+#                print(" " * self.width[0], end="")
+#                for secondindex in range(0, self.height):
 #                    print("#", end="")
 #                print("")
 #
