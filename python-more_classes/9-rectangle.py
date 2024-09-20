c8cc8131a6e2d2a70fbac0e7c7d08df43e9bb4c9
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-09-20 14:47:18 +0200

    Fix:
    Exchanging attribute order to allow test priority

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -20,8 +20,8 @@ class Rectangle:
         - a privtate width attribute
         """
 
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
         print_symbol = Rectangle.print_symbol
 
