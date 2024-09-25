114a45833eb2dea0d1971306a4050dfcb44c8901
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-09-25 12:04:18 +0200

    Create: Add BaseGeometry class
    - Created new file "base_geometry.py"
    - Implemented BaseGeometry class as an empty class
    - Added module and class docstrings

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module that hold the BaseGeometr class and
+will hold its methods and potential subclasses
+"""
+
+
+class BaseGeometry:
+    """
+    Class that will hold several methods and potential
+    subclasses about geometry.
+    """
+
+    pass
