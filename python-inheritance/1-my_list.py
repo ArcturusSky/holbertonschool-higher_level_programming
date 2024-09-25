d20b12268576d1097318c4127b6cda656e05a56a
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-09-25 10:20:57 +0200

    Feat: Implement MyList class with print_sorted method
    - Created MyList class that inherits from built-in list
    - Added print_sorted method to display list in ascending order
    - Utilized built-in sorted() function for efficient sorting
    - Included docstrings for module, class, and method documentation

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module retrieve a list from inherited list and sort
+it in ascending order
+"""
+
+class MyList(list):
+    """
+    Class that inherits from "list"
+    """
+
+    def print_sorted(self):
+        """
+        Function that prints the list but in ascending order
+        """
+        
+        print(sorted(self))
+
