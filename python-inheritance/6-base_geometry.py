4382555f058bae3f159c674a271a066506558df6
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-09-25 12:10:45 +0200

    Update: Add area method to BaseGeometry class
    - Added area method to BaseGeometry class
    - Implemented exception raising for unimplemented area method
    - Updated class docstring to reflect new method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module that hold the BaseGeometr class and
+will hold its methods and potential subclasses
+"""
+
+
+class BaseGeometry:
+    """
+    Class that will hold several methods and potential
+    subclasses about geometry.
+    """
+
+    def area(self):
+        """
+        Will probably be used to def area later
+        for now only raise exception
+        """
+
+        raise Exception("area() is not implemented")
