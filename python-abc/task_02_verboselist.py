7fee80433bfc7f7f00eb70cc45b1684e36bbcaa3
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-09-27 12:28:33 +0200

    Create: Add VerboseList class extending built-in list
    - Created new file "verbose_list.py"
    - Implemented VerboseList class inheriting from built-in list
    - Added verbose append method with custom message
    - Added verbose extend method with item count message
    - Added verbose remove method with error handling
    - Added verbose pop method with custom message
    - Included module and class docstrings

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+Module to demonstrate extending python built-in
+"""
+
+
+class VerboseList(list):
+    """
+    Class that inherits from list to desponstrate the possibility
+    of extending built-in.
+    """
+
+    def append(self, item):
+        """
+        Method to append an item but with verbose
+        """
+
+        print("Added [{}] to the list".format(item))
+        super().append(item)
+
+    def extend(self, x):
+        """
+        Method to extend the but with verbose
+        """
+
+        print("Extended the list with [{}] items.".format(len(x)))
+        super().extend(x)
+
+    def remove(self, item):
+        """
+        Method to remove an item but with verbose
+        """
+
+        if item not in self or item is None:
+            return None
+
+        else:
+            print("Removed [{}] from the list.".format(item))
+            super().remove(item)
+
+    def pop(self, index=-1):
+        """
+        Method to pop an item but with verbose
+        """
+        item = self[index]
+        print("Popped [{}] from the list.".format(item))
+        super().pop(index)
