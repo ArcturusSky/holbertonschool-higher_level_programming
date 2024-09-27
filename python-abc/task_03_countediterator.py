2222be3c028b70204a300ef8acdd1c5d6cbc6511
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-09-27 22:01:45 +0200

    Update: Enhance CountedIterator with exception handling and add test code
    - Modified __next__ method to handle StopIteration exception
    - Added if __name__ == "__main__" block for test code
    - Implemented test code to demonstrate CountedIterator functionality
    - Created a sample list and CountedIterator instance
    - Added a loop to iterate through the CountedIterator and print results
    - Included exception handling for StopIteration in test code

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -23,21 +23,25 @@ class CountedIterator():
         """
         Method that returns the next item and count
         """
-        self.counter += 1
-        next_item = next(self.iterator)
-        return next_item
+        try:
+            next_item = next(self.iterator)
+            self.counter += 1
+            return next_item
+        except StopIteration:
+            raise StopIteration("End of iterator reached.")
 
     def get_count(self):
         """ Method that returns number of items returnes"""
         return self.counter
 
+# Test code to run when module is executed as a script
+if __name__ == "__main__":
+    test_counter = [1, 2, 4, 8, 16]
+    counted_iter = CountedIterator(test_counter)
 
-test_counter = [1, 2, 4, 8, 16]
-counted_iter = CountedIterator(test_counter)
-
-try:
-    while True:
-        item = next(counted_iter)
-        print(f"Got {item}, total {counted_iter.get_count()} items iterated.")
-except StopIteration:
-    print("No more items.")
+    try:
+        while True:
+            item = next(counted_iter)
+            print(f"Got {item}, total {counted_iter.get_count()} items iterated.")
+    except StopIteration:
+        print("No more items.")
