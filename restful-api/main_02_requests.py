d54566f8098a7032127272712bafe94411e0577a
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-10-10 13:44:16 +0200

    Create: Add API interaction module for fetching and saving posts
    - Created new file "api_interaction.py"
    - Implemented fetch_and_print_posts function to retrieve and display post titles
    - Added fetch_and_save_posts function to fetch posts and save as CSV
    - Used requests library for API calls to JSONPlaceholder
    - Implemented CSV writing functionality using the csv module
    - Added error handling for API response status
    - Included module and function docstrings explaining functionality

---
+++ restful-api/main_02_requests.py
@@ -0,0 +1,44 @@
+"""
+Module to start to understand API
+"""
+import requests
+import csv
+
+def fetch_and_print_posts():
+    """
+    Fetching information, going through it and printing only the titles
+    """
+    # Make a GET resquest to jsplaceholder
+    response = requests.get("https://jsonplaceholder.typicode.com/posts") 
+
+    print("{}".format(response.status_code))
+
+    # Check the status code of the response
+    if response.status_code == 200:
+        data = response.json()
+        for post in data:
+            print(post['title'])
+
+
+def fetch_and_save_posts():
+    """
+    Fetching and saving as a dictionnary and serialized it into CSV
+    """
+
+    # Make a GET resquest to jsplaceholder
+    response = requests.get("https://jsonplaceholder.typicode.com/posts") 
+
+    # If successful converting data into a dictionnary
+    if response.status_code == 200:
+        data = response.json()
+        posts = [{"id": post["id"], "title": post["title"],"body": post["body"]} for post in data]
+
+    # Saving it into CSV
+    with open('posts.csv', mode='w', newline='', encoding='utf-8') as file:
+        fieldname = ["id", "title", "body"]
+        savepost = csv.DictWriter(file, fieldnames=fieldname)
+        savepost.writeheader() # Write header
+        savepost.writerows(posts) # Write multiple rows
+
+if __name__ == "__main__":
+    fetch_and_save_posts
