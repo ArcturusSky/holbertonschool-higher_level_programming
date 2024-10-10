d3382e2f1fe75fb5c1e2a6bbc17bbb3fe692a019
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-10-10 14:20:09 +0200

    Pycodestyle fix

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -2,15 +2,18 @@
 """
 Module to start to understand API
 """
+
+
 import requests
 import csv
+
 
 def fetch_and_print_posts():
     """
     Fetching information, going through it and printing only the titles
     """
     # Make a GET resquest to jsplaceholder
-    response = requests.get("https://jsonplaceholder.typicode.com/posts") 
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
 
     print("Status Code: {}".format(response.status_code))
 
@@ -27,19 +30,21 @@ def fetch_and_save_posts():
     """
 
     # Make a GET resquest to jsplaceholder
-    response = requests.get("https://jsonplaceholder.typicode.com/posts") 
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
 
     # If successful converting data into a dictionnary
     if response.status_code == 200:
         data = response.json()
-        posts = [{"id": post["id"], "title": post["title"],"body": post["body"]} for post in data]
+        posts = [{"id": post["id"], "title": post["title"],
+                  "body": post["body"]} for post in data]
 
     # Saving it into CSV
     with open('posts.csv', mode='w', newline='', encoding='utf-8') as file:
         fieldname = ["id", "title", "body"]
         savepost = csv.DictWriter(file, fieldnames=fieldname)
-        savepost.writeheader() # Write header
-        savepost.writerows(posts) # Write multiple rows
+        savepost.writeheader()  # Write header
+        savepost.writerows(posts)  # Write multiple rows
+
 
 if __name__ == "__main__":
     fetch_and_save_posts()
