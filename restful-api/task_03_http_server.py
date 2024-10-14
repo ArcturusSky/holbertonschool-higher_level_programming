4b866189d6b601e85010ac2913fca7da35cb1d36
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-10-14 15:45:46 +0200

    Create: Add simple API using Python's http.server module
    - Created new file "simple_api.py"
    - Implemented SimpleAPI class extending BaseHTTPRequestHandler
    - Added GET request handling for '/', '/data', and '/status' endpoints
    - Included JSON response capability for '/data' endpoint
    - Implemented 404 error handling for undefined endpoints
    - Added run function to start the server on port 8000
    - Included module and class docstrings explaining functionality

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+Module to start Develop a simple API using Python with the `http.server` module
+"""
+
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+
+class SimpleAPI(BaseHTTPRequestHandler):
+    """
+    Subclass to config a HTTP server
+    """
+
+    def do_GET(self):
+        """
+        
+        """
+
+        if self.path == '/':
+            self.send_response(200)  # 200 OK
+            self.send_header('Content-type', 'text/plain')  # Type de contenu
+            self.end_headers()  # Fin des en-têtes
+            self.wfile.write(b'Hello, this is a simple API!')
+            # Réponse aux client avec "b" pour byte, requis pour écrire la réponse
+
+        elif self.path == '/data':
+            self.send_response(200)  # 200 OK
+            self.send_header('Content-type', 'application/json')  # Type de contenu JSON
+            self.end_headers()  # Fin des en-têtes
+            # Créer un dictionnaire et le convertir en JSON
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode())  # Écrire la réponse en JSON
+
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(b'OK')
+        
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Endpoint not found')
+
+
+# Function pour démarrer le serveur
+def run(server_class=HTTPServer, handler_class=SimpleAPI, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Serving on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
