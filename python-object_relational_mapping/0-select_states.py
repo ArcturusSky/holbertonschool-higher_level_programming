a44fb7129f7dd0624bea1b6084d4f46af8f64ad6
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-11-11 13:16:16 +0100

    [FIX] Typo fix in sys.argv
    Used () instead of []

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,9 +7,9 @@ import sys
 
 if __name__ == "__main__":
     # Allocate given arguments as variable to be easier to be understood
-    username = sys.argv(1)
-    password = sys.argv(2)
-    database_name = sys.argv(3)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
 
     # Connecting to the MySQL database
     db = MySQLdb.connect(
